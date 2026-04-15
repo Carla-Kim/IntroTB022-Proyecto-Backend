8c27a67c552c5e3efa5c0bd6c8e866651bc73f35
Luis <fernander200@outlook.com>
2026-04-15 12:21:34 -0300

    in process: errors, pagination, validations, among others

--- app/partidos/routes.py
+++ app/partidos/routes.py
@@ -1,6 +1,40 @@
 from flask import Blueprint, request, jsonify
-#from .service import COMPLETAR ACA CON LAS FUNCIONES DEL SERVICE
+from app.partidos.service import PartidosService
+from app.utils.errors import BadRequestError
+from app.utils.validations import validate
 
 partidos_bp = Blueprint('partidos', __name__)
 
-#Enpoints
+@partidos_bp.get("/partidos")
+def listar_partidos():
+    equipo = request.args.get("equipo")
+    fecha = request.args.get("fecha")
+    fase = request.args.get("fase")
+    limit = request.args.get("_limit", type=int, default=10)
+    offset = request.args.get("_offset", type=int, default=0)
+
+    errors = validate_pagination(limit, offset)
+
+    if errors:
+        raise BadRequestError(errors=errors)
+
+    base_url = request.base_url
+    args = {
+        k: v for k, v in {
+            "equipo": equipo,
+            "fecha": fecha,
+            "fase": fase
+        }.items() if v is not None
+    }
+
+    result = PartidosService.listar_partidos(
+        equipo=equipo,
+        fecha=fecha,
+        fase=fase,
+        limit=limit,
+        offset=offset,
+        base_url=base_url,
+        args=args
+    )
+
+    return jsonify(result), 200
--- app/partidos/service.py
+++ app/partidos/service.py
@@ -1,1 +1,26 @@
-# from .model import COMPLETAR ACA CON LAS FUNCIONES DEL MODEL
+from app.partidos.model import PartidosModel
+from app.utils.pagination import build_links
+
+class PartidosService:
+
+    @staticmethod
+    def listar_partidos(base_url, args, equipo=None, fecha=None, fase=None, limit=10, offset=0):
+        data = PartidosModel.fetch_partidos(equipo, fecha, fase, limit, offset)
+
+        partidos = [
+            {
+                "id": d["id_partido"],
+                "equipo_local": d["equipo_local"],
+                "equipo_visitante": d["equipo_visitante"],
+                "fecha": str(d["fecha"]),
+                "fase": d["fase"]
+            }
+            for d in data["rows"]
+        ]
+
+        count = data["count"]
+
+        return {
+          "partidos": partidos,
+          "_links": build_links(base_url, args, limit, offset, count)
+        }
