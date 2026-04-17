ae4be56612284ffef1ac641ab58f951e7d08d5ef
jlima-boop <jlima@fi.uba.ar>
2026-04-17 17:58:26 -0300

    routes de partido
    Added endpoint to update match results.

--- app/partidos/routes.py
+++ app/partidos/routes.py
@@ -4,3 +4,10 @@ from flask import Blueprint, request, jsonify
 partidos_bp = Blueprint('partidos', __name__)
 
 #Enpoints
+from app.partido import service
+@partidos_bp.route("/partidos/<int:id_partido>/resultado", methods=["PUT"])
+
+def actualizando(id_partido):
+    data = request.get_json()
+    actualizado, code = service.actualizando_resultado(data, id_partido)
+    return jsonify(actualizado), code
