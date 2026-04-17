bc61a055b3c8daf854c111cc3522a6dc443810bb
jlima-boop <jlima@fi.uba.ar>
2026-04-17 17:56:47 -0300

    model de partido

--- app/partidos/model.py
+++ app/partidos/model.py
@@ -1,1 +1,14 @@
 from app.db import get_connection
+def actualizar_resultado(gol_local_act,gol_visit_act, id):
+    conn = get_connection()
+    cursor = conn.cursor(dictionary=True)
+    try:
+        sql = "UPDATE partidos SET goles_local = %s, goles_visitante = %s WHERE id_partido = %s"
+        cursor.execute(sql, (gol_local_act, gol_visit_act, id))
+        conn.commit()
+        return cursor.rowcount  
+    except Exception as e:
+        conn.rollback() 
+        raise e
+    finally:
+        cursor.close()
