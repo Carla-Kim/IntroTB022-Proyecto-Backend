ac40ea78351baf14b15c90d1b1fbcd558c615f63
jlima-boop <jlima@fi.uba.ar>
2026-04-17 18:02:55 -0300

    service de prediccion
    Added guardar_prediccion function to handle predictions.

--- app/prediccion/service.py
+++ app/prediccion/service.py
@@ -1,1 +1,24 @@
 # from .model import COMPLETAR ACA CON LAS FUNCIONES DEL MODEL
+from app.prediccion import model
+def guardar_prediccion(data, id_partido):
+    id_usuario = data.get("id usuario")
+    goles_locales = data.get("local")
+    goles_visitantes = data.get("visitante")
+    
+    partido_valido = model.verificacion(id_partido)
+    if partido_valido is None:
+        return {
+            "error": "El partido ya se jugó o no existe. No podés predecir."
+        }, 404 
+    try:
+        id_prediccion = model.guardando_prediccion(id_usuario, id_partido, goles_locales, goles_visitantes)   
+
+        return {
+            "proceso": "existoso",
+            "mensaje": "prediccion guardada correctamente",
+            "id_usuario": id_prediccion
+    }, 201
+    except Exception as e:
+        return {
+            "error": str(e)
+        }, 500
