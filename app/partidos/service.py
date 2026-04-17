df8423fa8e4764392077ee45d90a70a1f5d2302e
jlima-boop <jlima@fi.uba.ar>
2026-04-17 17:59:18 -0300

    service de partido

--- app/partidos/service.py
+++ app/partidos/service.py
@@ -1,1 +1,16 @@
 # from .model import COMPLETAR ACA CON LAS FUNCIONES DEL MODEL
+from app.partido import model
+def actualizando_resultado(data,id_partido):
+    gol_local = data.get("gol local")
+    gol_visitante = data.get("gol visitante")
+    gol_actualizado = model.actualizar_resultado(gol_local,gol_visitante, id_partido)
+    
+    if gol_actualizado == 0:
+        return {
+            "proceso" : "fallido",
+            "mensaje" : f"No se encontro el ID del partido {id_partido} o el resultado ya fue actualizado"
+        }, 404
+    return {
+        "proceso" : "existoso",
+        "mensaje" : f"el partido {id_partido} actualizado {gol_local}-{gol_visitante}  "
+    }, 200
