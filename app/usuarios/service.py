aa38a171e4807ffe3aee8e427cb128fc71253ce3
nicowestt <nico.westt@gmail.com>
2026-04-17 08:18:33 -0300

    Update usuarios service.py

--- app/usuarios/service.py
+++ app/usuarios/service.py
@@ -1,1 +1,4 @@
-# from .model import COMPLETAR ACA CON LAS FUNCIONES DEL MODEL
+from app.usuarios.model import eliminar_usuario_db
+
+def eliminar_usuario_service(id_usuario):
+   return eliminar_usuario_db(id_usuario)
