968706cabf2bd8991dba8e07fcb613bbd88f3777
nicowestt <nico.westt@gmail.com>
2026-04-17 08:28:51 -0300

    Update usuarios routes.py

--- app/usuarios/routes.py
+++ app/usuarios/routes.py
@@ -1,6 +1,16 @@
 from flask import Blueprint, request, jsonify
-#from .service import COMPLETAR ACA CON LAS FUNCIONES DEL SERVICE
+from app.usuarios.service import eliminar_usuario_service
 
 usuarios_bp = Blueprint('usuarios', __name__)
 
-#Enpoints
+@usuarios_bp.route('/usuarios/<int:id_usuario>', methods=['DELETE'])
+def eliminar_usuario(id_usuario):
+ try:
+    fue_eliminado = eliminar_usuario_service(id_usuario)
+    if fue_eliminado:
+       return jsonify({"mensaje": "Usuario eliminado correctamente"}), 200
+
+    return jsonify({"mensaje": "No existe ese usuario"}), 404
+ 
+ except Exception as e:
+       return jsonify({"mensaje": "Error interno del servidor"}), 500
