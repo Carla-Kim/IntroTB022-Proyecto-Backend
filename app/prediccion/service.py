cb5d6e54efff02243b1a5e5fe8b0bdd409a7142c
jlima-boop <jlima@fi.uba.ar>
2026-04-18 17:57:32 -0300

    muevo los codes a routes
    Refactor prediction saving logic to remove error handling and return structure.

--- app/prediccion/service.py
+++ app/prediccion/service.py
@@ -7,18 +7,10 @@ def guardar_prediccion(data, id_partido):
     
     partido_valido = model.verificacion(id_partido)
     if partido_valido is None:
-        return {
-            "error": "El partido ya se jugó o no existe. No podés predecir."
-        }, 404 
-    try:
-        id_prediccion = model.guardando_prediccion(id_usuario, id_partido, goles_locales, goles_visitantes)   
-
-        return {
+        return None
+    id_prediccion = model.guardando_prediccion(id_usuario, id_partido, goles_locales, goles_visitantes)   
+    return {
             "proceso": "existoso",
             "mensaje": "prediccion guardada correctamente",
             "id_usuario": id_prediccion
-    }, 201
-    except Exception as e:
-        return {
-            "error": str(e)
-        }, 500
+    }
