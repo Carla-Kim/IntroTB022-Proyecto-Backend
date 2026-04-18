9cae09df01f3fb3634f2b9998514d79ba854d1ac
jlima-boop <jlima@fi.uba.ar>
2026-04-18 17:55:27 -0300

    agrego los codes que estaban en service
    Add error handling and response for prediction endpoint.

--- app/prediccion/routes.py
+++ app/prediccion/routes.py
@@ -7,6 +7,12 @@ prediccion_bp = Blueprint('prediccion', __name__)
 from app.prediccion import service
 @prediccion_bp.route("/partidos/<int:id_partido>/prediccion", methods=["POST"])
 def guardar_prediccion(id_partido):
-    data = request.get_json()
-    resultado, code = service.guardar_prediccion(data, id_partido)
-    return jsonify(resultado), code
+    try:
+        data = request.get_json()
+        resultado = service.guardar_prediccion(data, id_partido)
+        if resultado is None:
+            return jsonify({"error": "El partido ya se jugó o no existe. No podés predecir."}), 400
+        return jsonify(resultado), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
