b4dd81ac963dd1e5f00575aa9f3ee0b89c8fba7e
nicowestt <nico.westt@gmail.com>
2026-04-18 07:46:42 -0300

    arreglo service.py de ranking

--- app/ranking/service.py
+++ app/ranking/service.py
@@ -1,4 +1,4 @@
 from app.ranking.model import obtener_ranking_db
 
-def obtener_ranking_service():
-  return obtener_ranking_db()
+def obtener_ranking_service(limit, offset):
+  return obtener_ranking_db(limit, offset)
