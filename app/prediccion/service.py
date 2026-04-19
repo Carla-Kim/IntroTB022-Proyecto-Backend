c5c665de0230bbea2a26129909a9332c39e137a3
jlima-boop <jlima@fi.uba.ar>
2026-04-18 21:27:04 -0300

    Handle incomplete request and exceptions in service.py
    lo complemente para los codigos 400,404 y 409

--- app/prediccion/service.py
+++ app/prediccion/service.py
@@ -5,12 +5,20 @@ def guardar_prediccion(data, id_partido):
     goles_locales = data.get("local")
     goles_visitantes = data.get("visitante")
     
+    if id_usuario is None or goles_locales is None or goles_visitantes is None:
+        return "solicitud incompleta"
+    
     partido_valido = model.verificacion(id_partido)
     if partido_valido is None:
-        return None
-    id_prediccion = model.guardando_prediccion(id_usuario, id_partido, goles_locales, goles_visitantes)   
-    return {
+        return "no encontrado"
+    try:
+        id_prediccion = model.guardando_prediccion(id_usuario, id_partido, goles_locales, goles_visitantes)   
+        return {
             "proceso": "existoso",
             "mensaje": "prediccion guardada correctamente",
             "id_usuario": id_prediccion
-    }
+        }
+    except Exception as e:
+        if "Duplicate entry" in str(e):
+            return "conflicto"
+        raise e
